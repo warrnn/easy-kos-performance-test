a1fd684ca7df6103d6de202826eacbf265a467e6
Bryan Warren <bryanwarren86@gmail.com>
2025-12-18 21:40:00 +0700

    add other page performance testing

--- locustfile.py
+++ locustfile.py
@@ -5,7 +5,7 @@ import re
 class BaseUser(HttpUser):
     abstract = True
     host = "http://127.0.0.1:8000"
-    wait_time = between(1, 3)
+    # wait_time = between(1, 3)
 
     username = None
     password = None
@@ -28,12 +28,15 @@ class BaseUser(HttpUser):
                 "username": self.username,
                 "password": self.password,
             },
-            allow_redirects=True,
+            allow_redirects=False,
         )
+        
+        print(token.group(1), self.username, self.password)
+        print(response.status_code, response.headers.get("Location"))
 
 
 class AdminUser(BaseUser):
-    weight = 1
+    # weight = 1
     username = "admin"
     password = "admin"
 
@@ -41,9 +44,13 @@ class AdminUser(BaseUser):
     def admin_dashboard(self):
         self.client.get("/admin")
 
+    @task
+    def admin_manage_users(self):
+        self.client.get("/admin/manage-users")
+
 
 class PemilikUser(BaseUser):
-    weight = 2
+    # weight = 2
     username = "adit"
     password = "1234"
 
@@ -51,9 +58,13 @@ class PemilikUser(BaseUser):
     def pemilik_dashboard(self):
         self.client.get("/pemilik_kos/index")
 
+    @task
+    def pemilik_laporan(self):
+        self.client.get("/pemilik_kos/laporan")
+
 
 class PenghuniUser(BaseUser):
-    weight = 5
+    # weight = 5
     username = "budi"
     password = "1234"
 
@@ -64,3 +75,7 @@ class PenghuniUser(BaseUser):
     @task
     def lihat_kamar(self):
         self.client.get("/penghuni/kos/index")
+
+    @task
+    def lihat_riwayat_pemesanan(self):
+        self.client.get("/penghuni/pemesanan/index")
